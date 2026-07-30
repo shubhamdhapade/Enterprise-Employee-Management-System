47520068de8ed01868028fcacc21078f05eb2a71
Shubham Dhapade <shubhamdhapade@gmail.com>
2026-07-30 14:04:12 +0530

    feat(EMS-007): configure Tailwind CSS

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -1,5 +1,11 @@
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Login Page
+      </h1>
+    </div>
+  );
 };
 
 export default LoginPage;
